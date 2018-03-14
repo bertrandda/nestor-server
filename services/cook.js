0684d73a3a1b2364729ff372125825949861811c
bertrandda <bertrandda@laposte.net>
2018-03-14 21:15:17 +0100

    Add season veg listing

--- services/cook.js
+++ services/cook.js
@@ -1,16 +1,29 @@
-let fs = require('fs');
+const fs = require('fs');
+const moment = require('moment-timezone');
 
 function getVegFood(req, res) {
     const replies = [];
+    const month = req.body.nlp.entities.datetime ? moment(req.body.nlp.entities.datetime[0].iso).get('month') + 1 : moment().get('month') + 1;
+    let vegList = JSON.parse(fs.readFileSync('./assets/data/fruits_veg.json', 'utf8'));
     if (req.body.nlp.entities.vegetable || req.body.nlp.entities.fruit) {
         const vegFood = req.body.nlp.entities.vegetable ? req.body.nlp.entities.vegetable[0] : req.body.nlp.entities.fruit[0];
-        let vegList = JSON.parse(fs.readFileSync('./assets/data/fruits_veg.json', 'utf8'));
-        vegList = vegList.filter(vegItem => compareVegNames(vegFood.value, vegItem.nameFr));
+        vegList = vegList.filter(vegItem => compareNames(vegFood.value, vegItem.nameFr));
 
-        if (vegList.length === 1 && vegList[0].month[new Date().getMonth() + 1]) {
+        if (vegList.length === 1 && vegList[0].month[month]) {
             replies.push({ type: 'text', content: `Le ${vegFood.value} est bien de saison` });
         } else {
             replies.push({ type: 'text', content: `Le ${vegFood.value} n'est pas de saison` });
+        }
+    } else if (req.body.nlp.entities.food_category) {
+        const category = req.body.nlp.entities.food_category[0].value;
+        if (compareNames(category, 'légume')) {
+            vegList = vegList.filter(vegItem => vegItem.type === 'vegetable' && vegItem.month[month]);
+            replies.push({ type: 'text', content: `Voilà la liste des légumes que j'ai trouvée :` });
+            replies.push({ type: 'text', content: `${vegList.map(vegItem => {return vegItem.nameFr}).join('\n')}` })
+        } else if (compareNames(category, 'fruit')) {
+            vegList = vegList.filter(vegItem => vegItem.type === 'fruit' && vegItem.month[month]);
+            replies.push({ type: 'text', content: `Voilà la liste des fruits que j'ai trouvée :` });
+            replies.push({ type: 'text', content: `${vegList.map(vegItem => {return vegItem.nameFr}).join('\n')}` })
         }
     }
 
@@ -19,7 +32,7 @@ function getVegFood(req, res) {
     })
 }
 
-function compareVegNames(input, model) {
+function compareNames(input, model) {
     model = model.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     const regex = new RegExp(`(?:^|\s)(${model}s?)(?=\s|$)`);
     return input.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').match(regex);
